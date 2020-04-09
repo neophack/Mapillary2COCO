80f1b1eeba096eaa421f64ad27738a5b50c4162c
neophack <pep3309531@163.com>
2020-04-09 11:19:02 +0800

    multi process with process bar

--- SplitTools/SplitTools.py
+++ SplitTools/SplitTools.py
@@ -10,30 +10,38 @@ import os
 import numpy as np
 # matplotlib.use('Agg')
 import scipy.misc
+import imageio
 from PIL import Image
+from tqdm import tqdm
+from multiprocessing.dummy import Pool as ThreadPool
+import concurrent.futures
+import time
 
-
+labels=[]
 def split_to_coco_creator(input_instance_array, labels):
 
     labelid_matrix_name = []
 
+    t0=time.time()
     label_image_info = np.array(input_instance_array / 256, dtype=np.uint8)
 
     instance_image_info = np.array(input_instance_array % 256, dtype=np.uint8)
-
+    t1=time.time()
 
     unique_label_info = np.unique(label_image_info)
     unique_instance_info = np.unique(instance_image_info)
-
+    t2=time.time()
+    #print("a",t2-t1,t1-t0)
     for label_id, label in enumerate(labels):
-
+        t00=time.time()
         if (label_id in (unique_label_info)) and (label["instances"] == True):
 
             each_label_array = np.zeros((input_instance_array.shape[0], input_instance_array.shape[1]),
                                         dtype=np.uint8)
 
             each_label_array[label_image_info == label_id] = 255
-
+            t01=time.time()
+            #print("b",t01-t00)
             for instance_id in range(256):
                 if (instance_id in unique_instance_info):
                     each_instance_array = np.zeros(
@@ -52,7 +60,7 @@ def split_to_coco_creator(input_instance_array, labels):
                             {"label_id": label_id, "instance_id": instance_id,
                              "label_name": label["readable"],
                              "image": final_instance_array})
-
+    #print("c",time.time()-t0)
     # each_id_array [(input_instance_array % 256) == instance_id] = 1
     # labelid_matrix_name.append ( (label_id , instance_id, label [ "readable"
     # ] , each_id_array) )
@@ -70,9 +78,9 @@ def split_dir(dir_name):
         config = json.load(config_file)
 
     labels = config['labels']
-
-    for file_name in files:
-        print("Task {}-{}".format(cnt,dir_name))
+    
+    def process(file_name):
+        print(file_name)
         file_name = file_name[:-4]
         instance_path = "{}/instances/{}.png".format(dir_name,file_name)
         instance_image = Image.open(instance_path)
@@ -83,7 +91,19 @@ def split_dir(dir_name):
         for item in image_label_instance_infomatrix:
             path = "{}_{}_{}.png".format(
                 file_name, item["label_name"].replace(" ", "_"), item["instance_id"])
-            scipy.misc.imsave("{}/annotations/{}".format(dir_name,path), item["image"])
+            imageio.imsave("{}/annotations/{}".format(dir_name,path), item["image"])
+        del image_label_instance_infomatrix
+        del instance_image
+        del instance_array
+        return 0
+    
+    def run(f, my_iter):
+        with concurrent.futures.ThreadPoolExecutor(2) as executor:
+            results = list(tqdm(executor.map(f, my_iter), total=len(my_iter)))
+    
+    run(process, files)
+
+        
 
 if __name__ == '__main__':
     split_dir("training")
